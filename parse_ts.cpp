47eaf32d13c099088c6a24d10a23dbf9bbd63436
Casey Mac <cmacdonald@vectormax.com>
2024-03-31 14:59:15 -0700

    wip

--- parse_ts.cpp
+++ parse_ts.cpp
@@ -2,13 +2,66 @@
 
 #include <iostream>
 #include <stdio.h>
+#include <stdint.h>
 
 using namespace std;
 
+bool pes_packet_details(unsigned char *buf, int idx) {
+
+    if (idx < 0)
+        return false;
+
+    int streamId, len, indicator, pesLen;
+    uint64_t pts=0, dts=0;
+
+    streamId = buf[idx];
+    len = (buf[idx+1] << 8) + buf[idx+2];
+    indicator = (buf[idx+4] >> 6);
+
+    idx += 5;
+    pesLen = buf[idx];
+
+    switch(indicator) {
+        case 2: // PTS only
+        case 3: // PTS and DTS
+            pts += (uint64_t)(buf[idx+1] & 0x0E) << 30;
+            pts += (uint64_t)(buf[idx+2] & 0xFF) << 22;
+            pts += (uint64_t)(buf[idx+3] & 0xFE) << 14;
+            pts += (uint64_t)(buf[idx+4] & 0xFF) <<  7;
+            pts += (uint64_t)(buf[idx+5] & 0xFE) >>  1;
+
+            if (indicator == 2) break;
+            
+            idx += 5;
+            dts += (uint64_t)(buf[idx+1] & 0x0E) << 30;
+            dts += (uint64_t)(buf[idx+2] & 0xFF) << 22;
+            dts += (uint64_t)(buf[idx+3] & 0xFE) << 14;
+            dts += (uint64_t)(buf[idx+4] & 0xFF) <<  7;
+            dts += (uint64_t)(buf[idx+5] & 0xFE) >>  1;
+
+            break;
+
+    }
+
+    if (streamId == 0xE0)
+        printf("streamId=%i Len=%i, pesLen=%i PTS=%li DTS=%li\n", streamId, len, pesLen, pts, dts);
+
+    return true;
+}
+
+int search_start_code(unsigned char *buf, int len) {
+
+    for(int i = 0; i < len - 3; i++) 
+        if ((buf[i] == 0x00) && (buf[i+1] == 0x00) && (buf[i+2] == 0x01) )
+            return i + 3;
+
+    return -1;
+}
+
 bool parse_packet(FILE *fp) {
 
-    unsigned char buf[2];
-    int result;
+    unsigned char buf[2],buffer[185];
+    int result, idx;
 
     if ((result = fread(buf, 1, 2, fp)) != 2)
         return false;
@@ -16,7 +69,15 @@ bool parse_packet(FILE *fp) {
     int pusi = (buf[0] & 0x40) >> 6;
     int pid  = ((buf[0] & 0x1F) << 8) + buf[1];
 
-    printf("pusi=%i, pid=%i\n",pusi,pid);
+    if ((result = fread(buffer, 1, 185, fp)) != 185)
+        return false;
+
+    if (pusi) {
+        idx = search_start_code(buffer, 185);
+        pes_packet_details(buffer, idx);
+    }
+
+    //printf("pusi=%i, pid=%i, idx=%i\n",pusi,pid,idx);
 
     return true;
 }
@@ -44,8 +105,6 @@ do {
     if (sync == 0x47)
         if (!parse_packet(fp))
             break;
-    
-    break;
 
 } while (true);
 
